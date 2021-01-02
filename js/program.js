aca8b8e906cc4e6423d4b97821082e8e788e9891
elcesarandresco <elcesarandresco@DESKTOP-24JUM1U.localdomain>
2021-01-02 01:06:41 -0500

    se concretó el funcionamiento inicial de la web para que cree las formas a partir de los datos ingresados

--- js/program.js
+++ js/program.js
@@ -1,23 +1,55 @@
 /* Obtener datos a través de los formularios */
 
-var lines = document.getElementById("lineas")
-var clr = document.getElementById("color")
-var button = document.getElementById("boton")
+var lines = document.getElementById("lineas");
+var clr = document.getElementById("colorsito");
+var button = document.getElementById("boton");
+var clear = document.getElementById("c_canvas");
 
 /* añadir escuchador de eventos */
 boton.addEventListener("click", drawcanvas);
+clear.addEventListener("click", clearcanvas);
 
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var ancho = d.width;
 
+function canvasfunction(color, xinicial, yinicial, xfinal, yfinal) {
+    
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
 
+}
 
-
+function clearcanvas () {
+    
+    lienzo.clearRect(0, 0, d.width, d.height);
+    console.log("Se limpió canvas correctamente");
+}
 
 /* Crear función que dibuje en el canvas */
 function drawcanvas() {
+    
+    color = clr.value;
+    lineas = lines.value;
+    espacio = ancho / lineas
+    var i = 0;
+
+    for (i = 0; i <= lineas; i++) {
+
+        xi = espacio * i;
+        yf = espacio * (1 + i); 
+
+        canvasfunction(color, xi, 0, ancho, yf);
+
+    }
 
 
+    console.log("Número de líneas: " + lines.value);
+    console.log("Colorn de las líneas" + clr.value);
 
 }
 
