33068863fe285dcd113ed299d0a9cd5015e2c14d
elcesarandresco <elcesarandresco@DESKTOP-24JUM1U.localdomain>
2021-01-01 23:59:43 -0500

    se crearon estilos iniciales y se declararon variables a travésdel documento de javascript

---
+++ js/program.js
@@ -0,0 +1,24 @@
+/* Obtener datos a través de los formularios */
+
+var lines = document.getElementById("lineas")
+var clr = document.getElementById("color")
+var button = document.getElementById("boton")
+
+/* añadir escuchador de eventos */
+boton.addEventListener("click", drawcanvas);
+
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+
+
+
+
+
+/* Crear función que dibuje en el canvas */
+function drawcanvas() {
+
+
+
+}
+
+
